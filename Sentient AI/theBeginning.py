a6f84a1787bd2bbe8ae99b9ca374655a51d73121
IsaacPrice <iprice25250@gmail.com>
2023-08-12 15:43:51 -0400

    added to the sentient AI

--- Sentient AI/theBeginning.py
+++ Sentient AI/theBeginning.py
@@ -14,10 +14,8 @@ data = pd.read_excel('Sentient AI/labeledText.xlsx')
 # Get rid of the File Name column
 data = data.drop(['File Name'], axis=1)
 
-# Change the labels to 0, 1, 2 for negative, neutral, positive
-data['LABEL'] = data['LABEL'].replace(['Negative'], 0)
-data['LABEL'] = data['LABEL'].replace(['Neutral'], 1)
-data['LABEL'] = data['LABEL'].replace(['Positive'], 2)
+# Change the labels to 0, 0.5, 1 for negative, neutral, positive and making the labels floats
+data['KEY'] = data['LABEL'].replace(['negative', 'neutral', 'positive'], [0, 0.5, 1])
 
 # Get rid of any hastags from the comments
 data['Caption'] = data['Caption'].str.replace('#', '')
@@ -30,6 +28,37 @@ data['Caption'] = data['Caption'].apply(replace_at_mentions)
 print (data.head())
 
 # Tokenize the data
-tokenizer = Tokenizer(num_words=10000, oov_token='<OOV>')
+tokenizer = Tokenizer(num_words=10000000, oov_token='<OOV>')
 tokenizer.fit_on_texts(data['Caption'])
 
+# Get the word index
+word_index = tokenizer.word_index
+
+# Split the data into X_train, X_test, y_train, y_test
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(data['Caption'], data['KEY'], test_size=0.2, random_state=42)
+
+# Create the training data
+training_sequences = tokenizer.texts_to_sequences(X_train)
+training_padded = pad_sequences(training_sequences, maxlen=100, padding='post', truncating='post')
+
+testing_sequences = tokenizer.texts_to_sequences(X_test)
+testing_padded = pad_sequences(testing_sequences, maxlen=100, padding='post', truncating='post')
+
+# Create the labels
+training_labels = y_train.to_numpy()
+
+# Create the model
+model = Sequential()
+model.add(keras.layers.Embedding(10000000, 16, input_length=100))
+model.add(keras.layers.GlobalAveragePooling1D())
+model.add(keras.layers.Dense(16, activation='relu'))
+model.add(keras.layers.Dense(3, activation='softmax'))
+
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Train the model
+history = model.fit(training_padded, training_labels, epochs=30, verbose=1)
+
+# Save the model
+model.save('Sentient AI/sentimentModel.h5')
